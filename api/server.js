20249e4c3ca3d0d73bb2c88ca0d2fd3d648bc51e
Bright <dsorn2@gmail.com>
2019-05-02 15:11:48 +0700

    api complete to database

--- api/server.js
+++ api/server.js
@@ -3,6 +3,9 @@ var express = require('express');
 var app = express();
 //import mysql 
 var mysql = require('mysql');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 //use file with front end --> folder web
 app.use(express.static('../web'));
@@ -23,6 +26,8 @@ var con = mysql.createConnection({
 app.get('/test',function(req,res){
   console.log("route test");
   res.send('route tess');
+
+  
   
 
 });
@@ -30,46 +35,85 @@ app.get('/test',function(req,res){
 
 app.post('/register',function(req,res){
     console.log("---------Running Route register---------");
+    res.send("finsih post");
+    // json print obj
+    console.log(req.body);
+
+    var u  = req.body;
+    var state = "";
+    
 
     //connect db
     con.connect(function(err){
         if(err)throw err;
+        console.log("connect db already");
 
-        // check username
-        function checkUsername(us){
-            sqlselect = "select username from all_user where username = ?";
-            var values = us;
-            con.query(sqlselect,[[values]], function(error,result){
-  
-                if (error) throw error;
-                if (result.length<=0) {              
-                  return false};
-                return true
-            });
+        function checkUsername(u,callback){
+          sqlselect = "select username from all_user where username = ?";
+          var values = u.username;
+
+          
+          con.query(sqlselect,[[values]], function(error,result){
+
+              if (error) throw error;
+              var range = result.length;
+              console.log(range);
+              
+              if(range==0){
+                console.log('number: '+ range);
+                return callback(true);
+                
+                
+              }
+              else{
+                return callback(false);
+              }
+             
+             
+              
+              
+              
+          });
+      
         } 
-        //add user
-        function addUser(u){
 
-            sqladd = "insert into all_user (username,email,codenumber,number,job,password,repeatpassword) values ?"
-    
-            //from object
-            var values = [[]]
+        checkUsername(u,function(result){
+          console.log(result);
+          
+          if(result){
+            sqladd = "insert into all_user (username,email,codenumber,number,job,password,repeatpassword) values ?";
+
+            var values = [[u.username,u.email,u.codenumber,u.number,u.job,u.password,u.repeatpassword]]
             con.query(sqladd,[values],function(error,result){
                 if (error) throw error;
             });
+            console.log("add user complete");
+            state = "pass";
+            
+          
+            
+
+          }
+          else{
+            console.log("username already exist");
+            
+            state = "notpass";
+            
+          }
+          
+
+        });
+
+        console.log(state);
+        
+        
+       
+       
+        
+        
     
     
-          }
-          if(checkUsername(user)){
-            addUser(user);
-            res.send("Register complete");
-            console.log("register complete");
-          
-          }else{
-            res.send("This username is already exist");
-            console.log("this username is already exist");
-          
-          }
+         
 
 
     });
--- web/js/registerActive.js
+++ web/js/registerActive.js
@@ -1,18 +1,44 @@
 $(document).ready(() => {
   
   $("#regisbutton").click(function(){
-    console.log("clicked");
-    $.ajax({ 
-      url: '/test',
-      type: 'GET',
-      success: function(result){
-        console.log("ajax success");
-        
+    console.log("run function regis");
+    
+    const userobj = {};
+    userobj.username = $("#username").val();
+    userobj.email = $("#email").val();
+    userobj.codenumber = $("#codenumber").val();
+    userobj.number = $("#phone").val();
+    userobj.job = $("#job").val();
+    userobj.password = $("#password").val();
+    userobj.repeatpassword = $("#repeatpassword").val();
 
-      }
+    // convet to json
+    const userobj_json = JSON.stringify(userobj);
+    console.log(userobj_json);
+    
+
+    $.ajax({
+        type: 'post',
+        url: '/register',
+        data: userobj_json,
+        contentType: 'application/json',
+        // if api response it will run success
+        success: function(result) {
+          // check result object for what you returned
+          console.log("success");
+          console.log(userobj_json);
+          console.log(result);
+          
+          
+          
+          
+        },
+        error: function(error) {
+          // check error object or return error
+        }
+      });
+      console.log("end");
       
-      
-    });
     
     
     
